40b4c6b2f9879cd65e29d2688816a17a8b9092fd
KGB22 <gggggggyuu9@gmail.com>
2024-03-14 23:58:18 +0900

    240314
    for문을 사용하여 문제 풀어보기

---
+++ src/For1.java
@@ -0,0 +1,11 @@
+public class For1 {
+    public static void main(String[] args) {
+        // for 문을 이용해서 1부터 76까지의 합을 구하는 코드를 작성하자.
+        int sum = 0;
+        for(int i = 1; i <= 76; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+}
---
+++ src/For2.java
@@ -0,0 +1,13 @@
+public class For2 {
+    public static void main(String[] args) {
+        // 1부터 100까지 출력 후 거꾸로 100부터 1까지 출력하는 프로그램을 작성해보자.
+        for(int a = 1; a <= 100; a++) {
+            System.out.println(a);
+        }
+        for(int a = 100; a >= 1; a--) {
+            System.out.println(a);
+        }
+        
+    }
+
+}
