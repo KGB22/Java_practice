94e0fd8809096d44bb0ab2ebea7e930b6136091c
gyubbb <152342801+KGB22@users.noreply.github.com>
2024-03-17 00:55:19 +0900

    Update and rename For6.java to While.java

--- src/While.java
+++ src/While.java
@@ -1,4 +1,4 @@
-public class For6 {
+public class While {
     public static void main(String[] args) {
         // while문과 Math.random()메소드를 이용해서 2개의 주사위를 던졌을 때 나오는 눈을 (눈1, 눈2) 형태로 출력하고,
         // 눈의 합이 5가 아니면 계속 주사위를 던지고, 눈의 합이 5이면 실행을 멈추는 코드를 작성해보세요.
